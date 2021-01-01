da7121773f889960ff7f494e3db533ad54cfe396
onukichi <onuonu007@gmail.com>
2021-01-01 14:47:52 +0900

    first commit

---
+++ src/components/templates/KbnTaskDetailModal.vue
@@ -0,0 +1,1 @@
+<template><p>タスク詳細ページ</p></template>
---
+++ src/store/actions.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+  login: ({ commit }, authInfo) => {
+    return Auth.login(authInfo)
+      .then(({ token, userId }) => {
+        commit(types.AUTH_LOGIN, { token, userId })
+      })
+      .catch(err => { throw err })
+  },
+
+  fetchLists: ({ commit }) => {
+    throw new Error('fetchLists action should be implemented')
+  },
+
+  addTask: ({ commit }) => {
+    throw new Error('addTask action should be implemented')
+  },
+
+  updateTask: ({ commit }) => {
+    throw new Error('updateTask action should be implemented')
+  },
+
+  removeTask: ({ commit }) => {
+    throw new Error('removeTask action should be implemented')
+  },
+
+  logout: ({ commit }) => {
+    throw new Error('logout action should be implemented')
+  }
+}
